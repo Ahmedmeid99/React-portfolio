f49501c5acc58907d337a193326bc0367a9c6337
ahmedmeid99 <you@example.com>
2022-10-23 22:02:03 +0200

    add-lazy-loader

--- src/components/my-certificates/Cards.js
+++ src/components/my-certificates/Cards.js
@@ -18,26 +18,28 @@ const Cards = (props) => {
     const fetchingData = async () => {
         try {
             setLoding(true)
-            const responsive = await fetch('https://react-http-f4f9d-default-rtdb.firebaseio.com/portfoliocertificates.json')
-            if (!responsive.ok) {
-                throw new Error('something went wrong!')
-            }
-            const data = await responsive.json()
-            const certificatesArray = []
-            for (const key in data) {
-                certificatesArray.push({
-                    id: key,
-                    imgLink: data[key].imgLink,
-                    liveLink: data[key].liveLink,
-                    type: data[key].type,
-                })
-            }
-            // return projectsArray
-            dispatch(certificatesActions.setcertificates(certificatesArray))
+            setTimeout(async () => {
+                const responsive = await fetch('https://react-http-f4f9d-default-rtdb.firebaseio.com/portfoliocertificates.json')
+                if (!responsive.ok) {
+                    throw new Error('something went wrong!')
+                }
+                const data = await responsive.json()
+                const certificatesArray = []
+                for (const key in data) {
+                    certificatesArray.push({
+                        id: key,
+                        imgLink: data[key].imgLink,
+                        liveLink: data[key].liveLink,
+                        type: data[key].type,
+                    })
+                }
+                // return projectsArray
+                dispatch(certificatesActions.setcertificates(certificatesArray))
+                setLoding(false)
+            }, 5000)
         } catch (e) {
             console.log('error =>')
         }
-        setLoding(false)
     }
 
     return (
